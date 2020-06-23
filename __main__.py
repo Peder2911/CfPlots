932c05a47b124ceda8438cf4d3ee01772fd7d89b
peder2911 <pglandsverk@gmail.com>
2020-06-23 14:20:42 +0200

    Added testing / type checking watcher

---
+++ __main__.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+class TestTest(unittest.TestCase):
+    def test_something(self):
+        self.assertTrue(True)
+
+    """
+    def setUp(self):
+        self.goodData = goodData 
+        self.badData = badData 
+        """
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
